f8922779f3d254042214137d0d00825b9c9666dc
Alvin <alvin.tol@hotmail.com>
2022-06-29 22:46:01 -0600

    adds sizes to tailwind config and centers main divs to screen

--- src/App.js
+++ src/App.js
@@ -1,53 +1,58 @@
 import '../src/css/main.css';
 
 const App = () => {
+
+  const button = 'px-6 py-6 m-1 text-center shadow-xl'
+
   return (
-    <div className="app">
-      main body
-      <div id='left-container' className='grid gap-3 grid-cols-3 grid-rows-3"'>
-        <div className='px-6 py-6 m-1 text-center drop-shadow'>
-          Button1
-        </div>
-        <div className=''>
-          Button2
-        </div>
-        <div className=''>
-          Button3
-        </div>
-        <div className=''>
-          Button4
-        </div>
-        <div className=''>
-          Button5
-        </div>
-        <div className=''>
-          Button6
-        </div>
-        <div className=''>
-          Button7
-        </div>
-        <div className=''>
-          Button8
-        </div>
-        <div className=''>
-          Button9
-        </div>
-      </div>
-      <div id='right-container flex-col'>
-        <div className='' id='power'>
-          power
-        </div>
-        <div id='display'>
-          display
-          <div>
-            screen
+    <div className="app h-screen flex flex-col items-center justify-center">
+
+      <div className='container flex flex-col-reverse md:flex-row'>
+        <div id='left-container' className='grid gap-3 grid-cols-3 grid-rows-3"'>
+          <div className={button}>
+            Button1
+          </div>
+          <div className={button}>
+            Button2
+          </div>
+          <div className={button}>
+            Button3
+          </div>
+          <div className={button}>
+            Button4
+          </div>
+          <div className={button}>
+            Button5
+          </div>
+          <div className={button}>
+            Button6
+          </div>
+          <div className={button}>
+            Button7
+          </div>
+          <div className={button}>
+            Button8
+          </div>
+          <div className={button}>
+            Button9
           </div>
         </div>
-        <div id='slider'>
-          slider
-        </div>
-        <div className='' id='bank'>
-          bank?
+        <div id='right-container flex-col'>
+          <div id='power'>
+            power
+          </div>
+          <div id='display'>
+            display
+            <div>
+              screen
+            </div>
+          </div>
+          <div id='slider'>
+            slider
+          </div>
+          <div id='bank'>
+            bank?
+          </div>
         </div>
       </div>
     </div>
