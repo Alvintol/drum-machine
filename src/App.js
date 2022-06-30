f2218cc8f820d503c1e3ab26055581d0400b8d74
Alvin <alvin.tol@hotmail.com>
2022-06-29 23:47:17 -0600

    moves hardcode buttons html from app to ButtonsContainer

--- src/App.js
+++ src/App.js
@@ -2,39 +2,13 @@ import '../src/css/main.css';
 
 const App = () => {
 
-  const button = 'px-6 py-6 m-1 text-center shadow-xl'
+
 
   return (
     <div className="app h-screen flex flex-col items-center justify-center">
       <div className='container flex flex-col-reverse border-4 rounded-md  md:flex-row lg:mx-1/2'>
         <div id='button-container' className='grid gap-3 grid-cols-3 grid-rows-3 p-6 md:w-1/2'>
-          <div className='px-6 py-6 m-1 text-center shadow-xl'>
-            1
-          </div>
-          <div className={button}>
-            2
-          </div>
-          <div className={button}>
-            3
-          </div>
-          <div className={button}>
-            4
-          </div>
-          <div className={button}>
-            5
-          </div>
-          <div className={button}>
-            6
-          </div>
-          <div className={button}>
-            7
-          </div>
-          <div className={button}>
-            8
-          </div>
-          <div className={button}>
-            9
-          </div>
+          
         </div>
         <div id='switch-container' className='flex flex-col justify-around items-center h-64 md:w-1/2 md: h-full'>          <div id='power'>
           <div className='flex flex-row text-sm text-bold w-20 h-8 justify-start'>
--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -1,5 +1,43 @@
 const ButtonContainer = () => {
-  return
+  const buttonClass = 'px-6 py-6 m-1 text-center shadow-xl';
+  const buttons =
+    <>
+      <div className='px-6 py-6 m-1 text-center shadow-xl'>
+        1
+      </div>
+      <div className={buttonClass}>
+        2
+      </div>
+      <div className={buttonClass}>
+        3
+      </div>
+      <div className={buttonClass}>
+        4
+      </div>
+      <div className={buttonClass}>
+        5
+      </div>
+      <div className={buttonClass}>
+        6
+      </div>
+      <div className={buttonClass}>
+        7
+      </div>
+      <div className={buttonClass}>
+        8
+      </div>
+      <div className={buttonClass}>
+        9
+      </div>
+    </>
+
+
+  return (
+    <div
+      id='button-container'
+      className='grid gap-3 grid-cols-3 grid-rows-3 p-6 md:w-1/2'>
+      {buttons}
+    </div>)
 };
 
 export default ButtonContainer;
