4037afb8f1fd60c8048be2698e8a807e214b999a
Alvin <alvin.tol@hotmail.com>
2022-06-29 23:31:37 -0600

    changes left/right container ids to button/switch respectively

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="app h-screen flex flex-col items-center justify-center">
       <div className='container flex flex-col-reverse border-4 rounded-md  md:flex-row lg:mx-1/2'>
-        <div id='left-container' className='grid gap-3 grid-cols-3 grid-rows-3 p-6 md:w-1/2'>
+        <div id='button-container' className='grid gap-3 grid-cols-3 grid-rows-3 p-6 md:w-1/2'>
           <div className={button}>
             1
           </div>
@@ -36,7 +36,7 @@ const App = () => {
             9
           </div>
         </div>
-        <div id='right-container' className='flex flex-col justify-around items-center h-64 md:w-1/2 md: h-full'>          <div id='power'>
+        <div id='switch-container' className='flex flex-col justify-around items-center h-64 md:w-1/2 md: h-full'>          <div id='power'>
           <div className='flex flex-row text-sm text-bold w-20 h-8 justify-start'>
             <div className='p-1 align-center'>
               ON
