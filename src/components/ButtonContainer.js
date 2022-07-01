07e8906793268c0c857e6a97c8d6c041efc6459d
Alvin <alvin.tol@hotmail.com>
2022-07-01 18:26:36 -0500

    adds dynamic rendering of buttons into buttoncontainner

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -1,46 +1,23 @@
 import Button from './Button';
 const ButtonContainer = () => {
 
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const buttons = array.map((btn, i) => {
+    return (
+      <Button
+      key={i}
+      id={btn}
+      />
+    )
+  })
+
+
   return (
     <div
       id='button-container'
-      className='grid gap-3 grid-cols-3 grid-rows-3 p-6 md:w-1/2'>
-      <Button 
-      key={1}
-      id={1}
-      />
-      <Button 
-      key={2}
-      id={2}
-      />
-      <Button 
-      key={3}
-      id={3}
-      />
-      <Button 
-      key={4}
-      id={4}
-      />
-      <Button 
-      key={5}
-      id={5}
-      />
-      <Button 
-      key={6}
-      id={6}
-      />
-      <Button 
-      key={7}
-      id={7}
-      />
-      <Button 
-      key={8}
-      id={8}
-      />
-      <Button 
-      key={9}
-      id={9}
-      />
+      className=''>
+      {buttons}
     </div>)
 };
 
