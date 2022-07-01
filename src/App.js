aa836ff22ac729acab7d4ad84fec224d9e213a5e
Alvin <alvin.tol@hotmail.com>
2022-07-01 17:36:05 -0500

    restyles button component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Slider from './components/Slider';
 
 const App = () => {
 
-
-
   return (
     <div className="app h-screen flex flex-col items-center justify-center">
       <div className='container flex flex-col-reverse border-4 rounded-md  md:flex-row lg:mx-1/2'>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = ({id}) => {
   return (
-    <div className='border-solid px-6 py-6 m-1 text-center shadow-xl'>
+    <div className='text-center font-bold text-stone-200'>
       {id}
     </div>
   )
