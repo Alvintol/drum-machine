474b67e4bb5bd59a6d75147550881cebbb3f66d1
Alvin <alvin.tol@hotmail.com>
2022-07-01 18:45:03 -0500

    imports switchcontainer to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ButtonContainer from './components/ButtonContainer';
 import Display from './components/Display';
 import Switch from './components/Switch';
 import Slider from './components/Slider';
+import SwitchContainer from './components/SwitchContainer';
 
 const App = () => {
 
@@ -10,18 +11,7 @@ const App = () => {
     <div className="app h-screen flex flex-col items-center justify-center">
       <div className='container flex flex-col-reverse border-4 rounded-md  md:flex-row lg:mx-1/2'>
         <ButtonContainer />
-        <div id='switch-container' className='flex flex-col justify-around items-center h-64 md:w-1/2 md: h-full'>
-          <Switch
-            id='power'
-          />
-          <Display />
-          <Slider
-            id='volume'
-          />
-          <Switch
-            id='bank'
-          />
-        </div>
+        <SwitchContainer />
       </div>
     </div>
   );
--- src/components/SwitchContainer.js
+++ src/components/SwitchContainer.js
@@ -1,5 +1,18 @@
 const SwitchContainer = () => {
-return 
+  return (
+    <div id='switch-container' className='flex flex-col justify-around items-center h-64 md:w-1/2 md: h-full'>
+      <Switch
+        id='power'
+      />
+      <Display />
+      <Slider
+        id='volume'
+      />
+      <Switch
+        id='bank'
+      />
+    </div>
+  )
 };
 
 export default SwitchContainer;
