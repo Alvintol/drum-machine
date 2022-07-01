50b77a6899f7e523d2a401a10a97f9e7a74cbb4c
Alvin <alvin.tol@hotmail.com>
2022-07-01 18:48:47 -0500

    corrects display, switch and slider import paths

--- src/components/SwitchContainer.js
+++ src/components/SwitchContainer.js
@@ -1,10 +1,12 @@
-import Display from './components/Display';
-import Switch from './components/Switch';
-import Slider from './components/Slider';
+import Display from '../components/Display';
+import Switch from '../components/Switch';
+import Slider from '../components/Slider';
 
 const SwitchContainer = () => {
   return (
-    <div id='switch-container' className='flex flex-col justify-around items-center h-64 md:w-1/2 md: h-full'>
+    <div
+      id='switch-container'
+      className='flex flex-col justify-around items-center h-64 md:w-1/2 md: h-full'>
       <Switch
         id='power'
       />
