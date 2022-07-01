a0309001d25914810e16a46cdb429d497ee900a8
Alvin <alvin.tol@hotmail.com>
2022-07-01 17:20:59 -0500

    moves hardcode slider from app to Slider component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,5 +1,15 @@
-const Slider = () => {
-  return
+const Slider = ({ id }) => {
+
+  return (
+    <div id={id}>
+      <input
+        min='0'
+        max='1'
+        step='0.01'
+        type='range'
+      />
+    </div>
+  )
 };
 
 export default Slider;
