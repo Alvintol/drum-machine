9a0eafa63f028ce6c3c0c9cc5f8abbb8dc03b085
Alvin <alvin.tol@hotmail.com>
2022-07-01 19:16:55 -0500

    addds click handler to switches

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,6 +1,10 @@
 const Switch = ({id}) => {
+
   return (
-    <div id={id}>
+    <div 
+    id={id}
+    onClick={()=>console.log('CLICKED:', id)}
+    >
       <div className='flex flex-row text-sm text-bold w-20 h-8 justify-start border-solid border-2 border-stone-400 rounded-sm bg-slate-600'>
         <div className='p-1 align-center border-solid border-2 border-stone-300 rounded-sm bg-slate-600 text-stone-200'>
           ON
