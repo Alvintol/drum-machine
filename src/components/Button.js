59bbdc575cd0a45d0a1fb4f13bea23ad63b9d6fc
Alvin <alvin.tol@hotmail.com>
2022-07-08 10:17:25 -0600

    centers button component text and adds color

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ const Button = ({id}) => {
   
   return (
     <div 
-    className='text-center font-bold text-stone-200 border rounded-sm border-black w-32 h-32 m-0.5 shadow-xl'
+    className='text-center flex flex-col justify-center font-bold text-stone-200 bg-emerald-400 border rounded-sm border-black w-32 h-32 shadow-xl'
     onClick={() => console.log('CLICKED:', id)}
     >
       {id}
