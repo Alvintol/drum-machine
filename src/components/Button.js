e93f5bdff1b6ea4a09395470473939ba622a555b
Alvin <alvin.tol@hotmail.com>
2022-07-08 10:09:17 -0600

    corrects button component sizes

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ const Button = ({id}) => {
   
   return (
     <div 
-    className='text-center font-bold text-stone-200 border rounded-sm border-black my-1 shadow-xl'
+    className='text-center font-bold text-stone-200 border rounded-sm border-black w-32 h-32 m-0.5 shadow-xl'
     onClick={() => console.log('CLICKED:', id)}
     >
       {id}
