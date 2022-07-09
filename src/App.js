c03e260c883f890a6dd5d8936dc814ac721bfbcd
Alvin <alvin.tol@hotmail.com>
2022-07-09 12:20:11 -0600

    adds displayButton function

--- src/App.js
+++ src/App.js
@@ -10,26 +10,27 @@ const App = () => {
 
   const [state, setState] = useState(appState)
 
-  const togglePower = () => {
-    return state.power === 'ON' ?
+  const togglePower = () =>
+    state.power === 'ON' ?
       setState(prev => ({ ...prev, power: 'OFF' })) :
       setState(prev => ({ ...prev, power: 'ON' }))
-  }
 
-  const toggleBank = () => {
-    return state.bank === 'ON' ?
+
+  const toggleBank = () =>
+    state.bank === 'ON' ?
       setState(prev => ({ ...prev, bank: 'OFF' })) :
       setState(prev => ({ ...prev, bank: 'ON' }))
-  }
+
+  const displayButton = (id) => setState(prev => ({ ...prev, display: id }))
 
   return (
     <StateContext.Provider value={state}>
       <div className="app h-screen flex flex-col items-center justify-center">
         <div className='container bg-neutral-200 flex flex-col-reverse border-4 border-double border-slate-600 rounded-md justify-between md:flex-row lg:mx-1/2'>
           <ButtonContainer />
-          <SwitchContainer 
-          togglePower={togglePower}
-          toggleBank={toggleBank}
+          <SwitchContainer
+            togglePower={togglePower}
+            toggleBank={toggleBank}
           />
         </div>
       </div>
