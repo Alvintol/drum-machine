d991eb9b4cbb23173c2936894dd7b706e222571b
Alvin <alvin.tol@hotmail.com>
2022-07-09 14:35:02 -0600

    adds click handler for power/bank switches

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -5,13 +5,18 @@ const Switch = ({ id }) => {
   const toggleBank = useBankUpdate();
   const togglePower = usePowerUpdate();
 
+  const mainClass = 'flex flex-row text-sm text-bold w-20 h-8 border-solid border-2 border-stone-400 rounded-sm bg-slate-600'
+
+  const powerBtn = state.power === 'ON' ? `${mainClass} justify-start`: `${mainClass} justify-end`;
+  const bankBtn = state.bank === 'Heater' ? `${mainClass} justify-start`: `${mainClass} justify-end`;
+
   return (
     <div
       id={id}
       onClick={id === 'power' ? togglePower : toggleBank}
     >
-      <div className='flex flex-row text-sm text-bold w-20 h-8 justify-start border-solid border-2 border-stone-400 rounded-sm bg-slate-600'>
-        <div className='p-1 align-center border-solid border-2 border-stone-300 rounded-sm bg-slate-600 text-stone-200'>
+      <div className={id === 'power' ? powerBtn : bankBtn}>
+        <div className='p-1 align-center border-solid border-2 border-stone-400 rounded-sm bg-slate-600 text-stone-200'>
           {id === 'power' ? state.power : state.bank}
         </div>
       </div>
